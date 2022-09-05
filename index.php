ff4759151bbc5a7ff19e4186c1f8409953986b41
gfftr <gff.terl@gmail.com>
2022-09-05 22:45:54 +0100

    sidebar & nav file

--- index.php
+++ index.php
@@ -22,6 +22,10 @@ include "includes/navigation.php";
     <small>Secondary Text</small>
    </h1>
 
+
+
+
+
    <!-- First Blog Post -->
    <h2>
     <a href="#">Blog Post Title</a>
@@ -35,7 +39,7 @@ include "includes/navigation.php";
     Posted on August 28, 2013 at 10:00 PM
    </p>
    <hr>
-   <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+   <img class="img-responsive" src="https://via.placeholder.com/900x300" alt="">
    <hr>
    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore
     nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
@@ -45,53 +49,13 @@ include "includes/navigation.php";
 
    <hr>
 
-   <!-- Second Blog Post -->
-   <h2>
-    <a href="#">Blog Post Title</a>
-   </h2>
-   <p class="lead">
-    by
-    <a href="index.php">Start Bootstrap</a>
-   </p>
-   <p>
-    <span class="glyphicon glyphicon-time"></span>
-    Posted on August 28, 2013 at 10:45 PM
-   </p>
-   <hr>
-   <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-   <hr>
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum
-    tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit
-    voluptates!</p>
-   <a class="btn btn-primary" href="#">Read More
-    <span class="glyphicon glyphicon-chevron-right"></span>
-   </a>
 
-   <hr>
 
-   <!-- Third Blog Post -->
-   <h2>
-    <a href="#">Blog Post Title</a>
-   </h2>
-   <p class="lead">
-    by
-    <a href="index.php">Start Bootstrap</a>
-   </p>
-   <p>
-    <span class="glyphicon glyphicon-time"></span>
-    Posted on August 28, 2013 at 10:45 PM
-   </p>
-   <hr>
-   <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-   <hr>
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque
-    quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem.
-    Magni, ratione.</p>
-   <a class="btn btn-primary" href="#">Read More
-    <span class="glyphicon glyphicon-chevron-right"></span>
-   </a>
 
-   <hr>
+
+
+
+
 
    <!-- Pager -->
    <ul class="pager">
@@ -105,73 +69,11 @@ include "includes/navigation.php";
 
   </div>
 
-  <!-- Blog Sidebar Widgets Column -->
-  <div class="col-md-4">
 
-   <!-- Blog Search Well -->
-   <div class="well">
-    <h4>Blog Search</h4>
-    <div class="input-group">
-     <input type="text" class="form-control">
-     <span class="input-group-btn">
-      <button class="btn btn-default" type="button">
-       <span class="glyphicon glyphicon-search"></span>
-      </button>
-     </span>
-    </div>
-    <!-- /.input-group -->
-   </div>
 
-   <!-- Blog Categories Well -->
-   <div class="well">
-    <h4>Blog Categories</h4>
-    <div class="row">
-     <div class="col-lg-6">
-      <ul class="list-unstyled">
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-      </ul>
-     </div>
-     <!-- /.col-lg-6 -->
-     <div class="col-lg-6">
-      <ul class="list-unstyled">
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-       <li>
-        <a href="#">Category Name</a>
-       </li>
-      </ul>
-     </div>
-     <!-- /.col-lg-6 -->
-    </div>
-    <!-- /.row -->
-   </div>
+  <?php include "includes/sidebar.php" ?>
 
-   <!-- Side Widget Well -->
-   <div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium
-     odit aliquam repellat tempore quos aspernatur vero.</p>
-   </div>
 
-  </div>
 
  </div>
  <!-- /.row -->
